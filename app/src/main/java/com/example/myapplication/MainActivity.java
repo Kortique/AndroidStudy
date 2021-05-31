36218eb8e582f89e28b2d9042ac8e7de7f7a0b63
Kortique <kolobOK8>
2021-05-31 17:12:07 +0300

    Добавил компоненты к 1п. ДЗ

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,4 +9,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
